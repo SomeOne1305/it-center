10d6f339af1c4332dac20fd1cbcdb5d5fc3f4c67
murtazoxurramov <murtazoxurramov1@gmail.com>
2024-04-25 00:08:31 +0500

    avatars

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,43 +10,43 @@ const About = () => {
 					<h1 className='text-4xl my-3 mt-5 py-3 text-green-500 font-bold text-center uppercase'>
 						Bizning Jamoa
 					</h1>
-					<div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center gap-5'>
+					<div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center gap-5 py-3'>
 						<div className='flex flex-col items-center'>
 							<div className='size-60'>
 								<img
-									src='./me.jpg'
+									src='https://cdn3d.iconscout.com/3d/premium/thumb/software-developer-10961397-8772473.png?f=webp'
 									className='w-full h-full object-cover rounded-full '
 									alt=''
 								/>
 							</div>
 							<div className='w-full text-center py-2 my-2'>
-								<h2 className='text-xl font-bold'>Jonathan McCarthy</h2>
+								<h2 className='text-xl font-bold'>Soatov Xurshid</h2>
 								<span className='text-lg text-gray-500'>Web dasturchi</span>
 							</div>
 						</div>
 						<div className='flex flex-col items-center'>
 							<div className='size-60'>
 								<img
-									src='./me.jpg'
+									src='https://cdn3d.iconscout.com/3d/premium/thumb/project-manager-10961393-8772469.png'
 									className='w-full h-full object-cover rounded-full '
 									alt=''
 								/>
 							</div>
 							<div className='w-full text-center py-2 my-2'>
-								<h2 className='text-xl font-bold'>Jonathan McCarthy</h2>
-								<span className='text-lg text-gray-500'>Web dasturchi</span>
+								<h2 className='text-xl font-bold'>Choriyev Aziz</h2>
+								<span className='text-lg text-gray-500'>Proyekt Menejer</span>
 							</div>
 						</div>
 						<div className='flex flex-col items-center'>
 							<div className='size-60'>
 								<img
-									src='./me.jpg'
+									src='https://cdn3d.iconscout.com/3d/premium/thumb/game-developer-11409501-9189371.png?f=webp'
 									className='w-full h-full object-cover rounded-full '
 									alt=''
 								/>
 							</div>
 							<div className='w-full text-center py-2 my-2'>
-								<h2 className='text-xl font-bold'>Jonathan McCarthy</h2>
+								<h2 className='text-xl font-bold'>Qurbonnazarov Baxtiyor</h2>
 								<span className='text-lg text-gray-500'>Web dasturchi</span>
 							</div>
 						</div>
